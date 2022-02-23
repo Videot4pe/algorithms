582b3f14e7d78eb66247c9e33ea448817266be7e
Alexandr Kvasnikov <videot4pe@gmail.com>
2022-02-23 16:27:12 +0300

    3sum

--- src/3sum.ts
+++ src/3sum.ts
@@ -4,9 +4,40 @@
 */
 
 function threeSum(nums: number[]): number[][] {
-  return [[]]
+  nums = nums.sort((a, b) => a < b ? -1 : 1)
+  let res = []
+  let current = 0
+  for (current; current < nums.length - 2; current++) {
+    if (current > 0 && nums[current] === nums[current - 1]) {
+      continue
+    }
+    let [left, right] = [current + 1, nums.length - 1]
+    while (left < right) {
+      let sum = nums[left] + nums[right]
+      if (sum < -nums[current]) {
+        left++
+      } else if (sum > -nums[current]) {
+        right--
+      } else {
+        res.push([nums[current], nums[left], nums[right]])
+        left++
+        while (left < right && nums[left] === nums[left - 1]) {
+          left++
+        }
+      }
+    }
+  }
+  return res
 };
 
-test('basic', () => {
-  expect(threeSum([-1, 0, 1, 2, -1, -4])).toBe([[-1, -1, 2], [-1, 0, 1]]);
+test('Базовый случай', () => {
+  expect(threeSum([-1, 0, 1, 2, -1, -4])).toStrictEqual([[-1, -1, 2], [-1, 0, 1]]);
+});
+
+test('Пустой массив', () => {
+  expect(threeSum([])).toStrictEqual([]);
+});
+
+test('Одно число', () => {
+  expect(threeSum([0])).toStrictEqual([]);
 });
